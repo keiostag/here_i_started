9a5073399359f3b4717a71dbcdf055eadfeb1166
keiostag <71792279+keiostag@users.noreply.github.com>
2022-07-11 00:00:31 -0300

    Adicionado nova saída de texto: I'm alive?

--- src/PrimeiroPrograma.java
+++ src/PrimeiroPrograma.java
@@ -1,7 +1,11 @@
-/** Saída de texto: Hello World!*/
+/** Meu primeiro código*/
+
 class HelloWorld {
     public static void main(String[] args) {
+        /** Saída de texto: Hello World! **/
         System.out.println("Hello World!");
+        /** Saída de texto: I'm alive? **/
+        System.out.println("I'm alive?");
     }
 
 }
